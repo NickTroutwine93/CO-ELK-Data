9481f43d926f134052a8a3160618c13d76f62fd8
Nick Troutwine <nicktroutwine93@gmail.com>
2023-08-09 19:02:18 -0500

    Back to working not sure what was going on there

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ const mockData = require('./mock.json');
 app.get("/api", (req, res) => {
   let success = req.query.success
   let hunters = req.query.hunters;
+  console.log(mockData,success,hunters)
   var fakeResponse = pd.parsingGMUData(mockData,success,hunters);
+  console.log(fakeResponse,"s",success,"h",hunters)
   res.json({ message: fakeResponse });
 });
 
--- server/parsingGMUData.js
+++ server/parsingGMUData.js
@@ -1,8 +1,10 @@
 exports.parsingGMUData = function (data,success,hunters) {
-    var payload = "<table className='data-table'><thead><tr><th>unit</th><th>bulls</th><th>cows</th><th>calves</th><th>totalHarvest</th><th>totalHunters</th><th>PercSuccess</th><th>totalDays</th><tbody>";
+    var payload = "<table class='data-table'><thead><tr><th>unit</th><th>bulls</th><th>cows</th><th>calves</th><th>totalHarvest</th><th>totalHunters</th><th>PercSuccess</th><th>totalDays</th><tbody>";
     var length = data["units"].length;
+    var returnLenght = 0;
     for(var i = 0; i<length; i++){
         if(data["units"][i]["PercSuccess"]>=success&&data["units"][i]["totalHunters"]<=hunters){
+            returnLenght++;
             console.log(data["units"][i])
             var unit = data["units"][i]["unit"];
             var bulls = data["units"][i]["bulls"];
@@ -17,6 +19,6 @@ exports.parsingGMUData = function (data,success,hunters) {
         }
     }
     payload+="</tbody></table>"
-    console.log(length);
+    console.log(returnLenght);
     return payload;
 }
